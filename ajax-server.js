13422954867ea8d8e489e4d18a671acadb2742a3
ArtNazarov <admin@artnazarov.ru>
2016-05-27 13:08:15 +0500

    search using modifiers

--- ajax-server.js
+++ ajax-server.js
@@ -1,3 +1,42 @@
+function like(str, delim, field, notmodifier, oper){ 
+	if (str != "")
+	{
+		
+	return "("+str.split(delim).map(function(v){return " ( UPPER("+field+") "+notmodifier+ " LIKE '%"+v.toUpperCase()+"%' )  ";}).join(oper)+")";
+	}
+	else
+	{
+		return "";
+	};
+	
+};
+
+function like_wrapper(str, delim, field)
+{
+	
+	var AND_NOT_part = str.split(delim).filter( function(x) {return (x.charAt(0)=='-');}).map(function(v) {return v.substring(1);}).join(delim);
+	var OR_part = str.split(delim).filter( function(x) {return (x.charAt(0)!='-');}).join(delim);
+	var result = "";	
+	var left_side = like(AND_NOT_part, delim, field, ' NOT ', ' AND ');
+	var right_side = like(OR_part, delim, field, '', ' OR ');	
+	console.log(left_side);
+	console.log(right_side);
+  var result = "";
+	if ((left_side != "") && (right_side != ""))
+	{
+			result = left_side + ' AND '+right_side;
+	};
+	if ((left_side != "") && (right_side == ""))
+	{
+		result = left_side;
+	};
+	if ((left_side == "") && (right_side != ""))
+	{
+		result = right_side;
+	};
+	return result;
+};
+
 /* Response */
 var RowsCollection = function(a){this.rows = a;};
 RowsCollection.prototype.add = function(row) {this.getRows().push(row);};
@@ -68,21 +107,31 @@ app.get('/ajax', function(req, res){
 	var offset = (p-1) * 10;
 	var labels_part = "";
 	var category_part = "";
-	var caption_part = " 1=1 ";
+	var caption_part = "";
 	var ordering_part = " ORDER BY caption ";
+	
 	if (labels != "")
 	{
-		labels_part = " AND (UPPER(labels) LIKE '%"+labels+"%' ) "
+		labels_part = like_wrapper(labels, ',', 'labels');		
 	};
+	
 	if (request != "")
 	{
-	caption_part = " ( UPPER(caption) LIKE '%"+request+"%' ) "
+		caption_part = like_wrapper(request, ',', 'caption');
+		if (caption_part != "") {caption_part = caption_part + " AND ";}
+		else { caption_part = " (1=1) AND";};						
 	};	
+	
 	if (category != "")
 	{
-		category_part = " AND (UPPER(category) LIKE '%"+category+"%' ) "
-	};
-	var db_req = "SELECT * FROM data WHERE "+ caption_part + " " + category_part + " " + labels_part + ordering_part +" LIMIT 10 OFFSET "+String(offset);
+		category_part = like_wrapper(category, ',', 'category');
+	  if (category_part != "")
+		{
+				category_part = category_part + " AND ";
+		};
+	};	
+	
+	var db_req = "SELECT * FROM data WHERE "+ caption_part + category_part + labels_part + ordering_part +" LIMIT 10 OFFSET "+String(offset);
 	console.log(request);
 	console.log(offset);
 	console.log(db_req);
