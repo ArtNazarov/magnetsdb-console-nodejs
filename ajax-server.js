27420f07c8aa3355456792bc83639a4eda1675cf
ArtNazarov <admin@artnazarov.ru>
2016-06-01 21:52:56 +0500

    two modes

--- ajax-server.js
+++ ajax-server.js
@@ -1,8 +1,32 @@
-function like(str, delim, field, notmodifier, oper){ 
+function maybe_upper(str,  flag)
+{
+	 var result = "";
+	 if (!flag)
+	 {
+		 result = str;
+	 }
+	 else
+	 {
+		 result = str.toUpperCase();
+	 };
+	 //console.log(result);
+	 return result;
+}
+
+function like(str, delim, field, notmodifier, oper, umode){ 
 	if (str != "")
 	{
-		
-	return "("+str.split(delim).map(function(v){return " ( UPPER("+field+") "+notmodifier+ " LIKE '%"+v.toUpperCase()+"%' )  ";}).join(oper)+")";
+  
+	return "("+str.split(delim).map(function(v){
+		if (umode)
+		{
+			return 	" ( UPPER("+field+") "+notmodifier+ " LIKE '%"+maybe_upper(v, umode)+"%' )  ";
+		}
+		else
+		{
+			return 	" ( "+field+" "+notmodifier+ " LIKE '%"+maybe_upper(v, umode)+"%' )  ";
+		};			
+	}).join(oper)+")";
 	}
 	else
 	{
@@ -11,29 +35,23 @@ function like(str, delim, field, notmodifier, oper){
 	
 };
 
-function like_wrapper(str, delim, field)
+function like_wrapper(str, delim, field, umode)
 {
 	
 	var AND_NOT_part = str.split(delim).filter( function(x) {return (x.charAt(0)=='-');}).map(function(v) {return v.substring(1);}).join(delim);
-	var OR_part = str.split(delim).filter( function(x) {return (x.charAt(0)!='-');}).join(delim);
+	var OR_part = str.split(delim).filter( function(x) {return ((x.charAt(0)!='-')&&(x.charAt(0)!='+'));}).join(delim);
+	var AND_part = str.split(delim).filter( function(x) {return (x.charAt(0)=='+');}).map(function(v) { return v.substring(1);}).join(delim);
 	var result = "";	
-	var left_side = like(AND_NOT_part, delim, field, ' NOT ', ' AND ');
-	var right_side = like(OR_part, delim, field, '', ' OR ');	
-	console.log(left_side);
-	console.log(right_side);
-  var result = "";
-	if ((left_side != "") && (right_side != ""))
-	{
-			result = left_side + ' AND '+right_side;
-	};
-	if ((left_side != "") && (right_side == ""))
-	{
-		result = left_side;
-	};
-	if ((left_side == "") && (right_side != ""))
-	{
-		result = right_side;
-	};
+	var p = [];
+	var q;
+	q = like(AND_NOT_part, delim, field, ' NOT ', ' AND ', umode);
+	if (q != "") { p.push("(" + q + ")"); };
+	q = like(OR_part, delim, field, ' ', ' OR ', umode);	
+	if (q != "") { p.push("(" + q + ")"); };
+	q = like(AND_part, delim, field, ' ', ' AND ', umode);
+	if (q != "") { p.push("(" + q + ")"); };
+  result = p.join(' AND ');
+	console.log('['+result+']');
 	return result;
 };
 
@@ -80,6 +98,7 @@ var fs = require('fs');
 var db = new sqlite3.Database('main-sqlite.db');
 var url = require('url');
 var port = 44444;
+var upper_mode = false;
 
 
 var fetching = function (request)
@@ -133,19 +152,20 @@ app.get('/ajax', function(req, res){
 	if ('search' in query)
 	{
 	console.log(query.search);
-	request = query.search.toUpperCase();		
+	request = maybe_upper(query.search, upper_mode);
+	console.log(request);
 	};
 	var labels = "";
 	if ('labels' in query) 
 	{
 	console.log(query.labels);
-  labels = query.labels.toUpperCase();
+  labels = maybe_upper(query.labels, upper_mode);
 	};	
 	var category = "";
 	if ('category' in query) 
 	{
 	console.log(query.category);
-	category = query.category.toUpperCase();
+	category = maybe_upper( query.category, upper_mode);
 	};
 	
 	var p = 1;
@@ -161,18 +181,18 @@ app.get('/ajax', function(req, res){
 	
 	if (labels != "")
 	{
-		labels_part = like_wrapper(labels, ',', 'labels');		
+		labels_part = like_wrapper(labels, ',', 'labels', upper_mode);		
 	};
 	
 	if (request != "")
 	{
-		caption_part = like_wrapper(request, ',', 'caption');
+		caption_part = like_wrapper(request, ',', 'caption', upper_mode);
 		if (caption_part == "") { caption_part = " (1=1) "; };
 	};	
 	
 	if (category != "")
 	{
-		category_part = like_wrapper(category, ',', 'category');	  
+		category_part = like_wrapper(category, ',', 'category', upper_mode);	  
 	};	
 	
 	
